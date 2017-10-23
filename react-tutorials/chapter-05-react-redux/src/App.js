52a45b62fcac47f2fadcb3191e735bfafd19b4a4
Ngo Thanh Tung <tungnt@softech.vn>
2017-10-23 19:01:54 +0700

    Update chapter 05

--- react-tutorials/chapter-05-react-redux/src/App.js
+++ react-tutorials/chapter-05-react-redux/src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
 
 export default App;
--- react-tutorials/chapter-05-react-redux/src/index.js
+++ react-tutorials/chapter-05-react-redux/src/index.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
-import App from './components/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import reducer from './reducers';
