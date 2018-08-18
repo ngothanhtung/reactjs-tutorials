d403e7b5492447261c219e9f452d0486aff7007c
Ngo Thanh Tung <tungnt@softech.vn>
2018-08-18 10:35:16 +0700

    Update QUIZ

--- cpcit-examples/first-project/src/Quiz.js
+++ cpcit-examples/first-project/src/Quiz.js
@@ -1,42 +1,71 @@
 import React, { Component } from 'react';
 import QuestionItem from './QuestionItem';
 
+const questions = [
+  {
+    questionText: 'Chien thang Dien Bien Phu vao nam nao?',
+    options: [
+      { text: '1945', isCorrect: false },
+      { text: '1954', isCorrect: true },
+      { text: '1968', isCorrect: false },
+      { text: '1975', isCorrect: false }
+    ]
+  },
+  {
+    questionText: 'Tu nao sau day co nghia la con meo?',
+    options: [
+      { text: 'cat', isCorrect: true },
+      { text: 'dog', isCorrect: false },
+      { text: 'mouse', isCorrect: false },
+      { text: 'pig', isCorrect: false }
+    ]
+  }
+];
+
 class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions: [
-        {
-          questionText: 'Chien thang Dien Bien Phu vao nam nao?',
-          options: [
-            { text: '1945', isCorrect: false },
-            { text: '1954', isCorrect: true },
-            { text: '1968', isCorrect: false },
-            { text: '1975', isCorrect: false }
-          ]
-        },
-        {
-          questionText: 'Tu nao sau day co nghia la con meo?',
-          options: [
-            { text: 'cat', isCorrect: true },
-            { text: 'dog', isCorrect: false },
-            { text: 'mouse', isCorrect: false },
-            { text: 'pig', isCorrect: false }
-          ]
-        }
-      ]
+      questions: questions,
+      index: 0,
+      gameStatus: 'running'
     };
   }
 
   render() {
     return (
-      <div>
+      <div className="container" style={{ margin: 24 }}>
         {
-          this.state.questions.map((item, index) => (
-            <QuestionItem key={index} data={item} />
-          ))
-        }
+          this.state.gameStatus === 'running' &&
+          <div>
+            <QuestionItem questionIndex={this.state.index + 1} data={this.state.questions[this.state.index]} onAnswer={(result) => {
+              // LUU KET QUA:
+              this.state.questions[this.state.index].userAnswer = { result: result };
+              this.setState({ questions });
 
+              if (this.state.index < this.state.questions.length - 1) {
+                this.setState({ index: this.state.index + 1 });
+              }
+              else {
+                this.setState({ gameStatus: 'completed' });
+              }
+            }} />
+          </div>
+
+        }
+        {
+          this.state.gameStatus === 'completed' &&
+          <div>
+            <h3>Result:</h3>
+            <hr />
+            {
+              this.state.questions.map((item, index) => (
+                <QuestionItem key={index} questionIndex={index + 1} data={item} />
+              ))
+            }
+          </div>
+
+        }
       </div>
     );
   }
