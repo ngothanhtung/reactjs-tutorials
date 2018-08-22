dae33e6839a8200d6fdfcbb36b8fc8926f28dfe2
Ngo Thanh Tung <tungnt@softech.vn>
2018-08-22 17:59:15 +0700

    Update

---
+++ react-basic/02-react-workshop/src/components/AudioPlayerExamples/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class AudioPlayerExamples extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> AudioPlayerExamples </Text>
+      </View>
+    );
+  }
+}
