b6c82efd54957a9f8859028347ce2576a7a02aa3
Ngo Thanh Tung <tungnt@softech.vn>
2018-09-05 16:06:01 +0700

    Update Examples

--- react-basic/02-react-workshop/src/components/AxiosExamples/CallExpressjsAPIExamples.js
+++ react-basic/02-react-workshop/src/components/AxiosExamples/CallExpressjsAPIExamples.js
@@ -35,7 +35,6 @@ export default class CallExpressjsAPIExamples extends Component {
           })
             .then(response => {
               console.log(response.data);
-              this.setState({ products: response.data });
               this.setState({ loading: false });
             });
         }}>
@@ -45,17 +44,20 @@ export default class CallExpressjsAPIExamples extends Component {
           this.state.loading &&
           <h1>Loading ...</h1>
         }
-
-        <table>
-          {
-            (this.state.loading === false) && this.state.products.map((item, index) => (
-              <tr key={index}>
-                <td>
-                  {item._id}
-                </td>
-              </tr>
-            ))
-          }
+        <hr />
+        <table className="table table-bordered">
+          <tbody>
+            {
+              (this.state.loading === false) && this.state.products.map((item, index) => (
+                <tr key={index}>
+                  <td>{item._id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.discount}</td>
+                </tr>
+              ))
+            }
+          </tbody>
         </table>
       </div>
     );
