660b503764a62c675fbd95c6308c402b14cd4933
Ngo Thanh Tung <tungnt@softech.vn>
2018-09-10 15:15:06 +0700

    Update Theme

--- exercises/quiz-application/src/components/LoginName.js
+++ exercises/quiz-application/src/components/LoginName.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loginAsync } from '../modules/quiz/actions/loginActions';
-import { Avatar } from 'antd';
-
-const avatarUrl = 'https://edusynch.s3.amazonaws.com/uploads/student/avatar/102514/thumb_23172429_10156831208144676_2528946045023983681_n.jpg';
+import { Avatar, Button } from 'antd';
 
 class LoginName extends Component {
   constructor(props) {
@@ -24,9 +22,14 @@ class LoginName extends Component {
         {
           this.props.loggedInUser &&
           <div style={{ display: 'flex', alignItems: 'center' }}>
-            <Avatar size="small" src={avatarUrl} />
+            <Avatar size="small" src={this.props.loggedInUser.avatarUrl} />
             <div style={{ width: 4 }}></div>
             <strong>Welcome {this.props.loggedInUser.fullName}!</strong>
+
+            <Button onClick={() => {
+              localStorage.clear();
+              window.location.href = '/';
+            }}>Logout</Button>
           </div>
 
         }
--- exercises/quiz-backend/routes/users.js
+++ exercises/quiz-backend/routes/users.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/login', function (req, res, next) {
   const { email, password } = req.body;
-  var result = { id: 1, email: email, fullName: 'Ngô Thanh Tùng', roles: [1, 5, 7, 9] };
+  var result = {
+    id: 1,
+    email: email,
+    fullName: 'Ngô Thanh Tùng',
+    avatarUrl: 'https://edusynch.s3.amazonaws.com/uploads/student/avatar/102514/thumb_23172429_10156831208144676_2528946045023983681_n.jpg',
+    roles: [1, 5, 7, 9]
+  };
   // res.json(result);
   setTimeout((() => { res.json(result) }), 2000);
 });
