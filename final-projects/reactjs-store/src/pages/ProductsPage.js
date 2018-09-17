f1d9f227408992d056aac3d7137cc17f7ad78e08
Ngo Thanh Tung <tungnt@softech.vn>
2018-09-17 16:12:38 +0700

    Update Categories

--- final-projects/reactjs-store/src/pages/ProductsPage.js
+++ final-projects/reactjs-store/src/pages/ProductsPage.js
@@ -9,14 +9,17 @@ export default class ProductsPage extends Component {
     };
   }
 
-  componentWillMount() {
-    const id = this.props.match.params.id;
-    const url = `http://localhost:9000/categories/${id}/products`;
-    axios.get(url).then(response => {
-      console.log('PRODUCTS', response.data);
-      this.setState({ products: response.data });
-    });
+  componentDidUpdate(nextProps, nextState) {
+    if (nextProps.match !== this.props.match) {
+      const id = this.props.match.params.id;
+      const url = `http://localhost:9000/categories/${id}/products`;
+      axios.get(url).then(response => {
+        console.log('PRODUCTS', response.data);
+        this.setState({ products: response.data });
+      });
+    }
   }
+
 
   render() {
     return (
