09c109c56dd9c3ea87e979b20857ef90bf0bc301
Ngo Thanh Tung <tungnt@softech.vn>
2020-08-23 02:01:33 +0700

    Update StoryBook

--- react-storybook/src/stories/MButton.stories.js
+++ react-storybook/src/stories/MButton.stories.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import { Button } from '@material-ui/core';
+import { Button as MButton } from '@material-ui/core';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: { main: purple[500] }, // Purple and green play nicely together.
-    secondary: { main: '#11cb5f' }, // This is just green.A700 as hex.
-  },
-  typography: { useNextVariants: true },
-});
+const Button = (props) => {
+  return <MButton {...props}>{props.label}</MButton>;
+};
 
 export default {
   title: 'Material/Button',
