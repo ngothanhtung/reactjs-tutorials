7dcaa7193d1ffa033e90ee31f4ce8e54878af64c
Ngo Thanh Tung <tungnt@softech.vn>
2022-05-24 15:18:50 +0700

    Update example

--- react-basic/01-a-react-get-started/like-button.js
+++ react-basic/01-a-react-get-started/like-button.js
@@ -12,19 +12,11 @@ class LikeButton extends React.Component {
     if (this.state.liked) {
       return 'You liked this.';
     }
-    return (
-      <button onClick={() => this.setState({ liked: true })}>Like</button>
-    )
 
-
-
-    // return e(
-    //   'button',
-    //   { onClick: () => this.setState({ liked: true }) },
-    //   'Like'
-    // );
+    return e('button', { onClick: () => this.setState({ liked: true }) }, 'Like');
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
