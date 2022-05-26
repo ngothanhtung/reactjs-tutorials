b7df54f58167978a0f14136b5b4b0234b00f8fa1
Ngo Thanh Tung <tungnt@softech.vn>
2022-05-26 10:52:23 +0700

    Update Homeworks

--- 2-Examples/react-examples/src/Homeworks/Session01/ChartBar/index.js
+++ 2-Examples/react-examples/src/Homeworks/Session01/ChartBar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
+import './styles.css';
 
 function ChartBar({ text = 'TEXT', color1 = 'violet', color2 = 'pink', percentage = 50 }) {
   return (
@@ -8,7 +9,7 @@ function ChartBar({ text = 'TEXT', color1 = 'violet', color2 = 'pink', percentag
         <span className={styles.text}> {text}</span>
       </div>
       <div className={styles.column_right}>
-        <div className={styles.column_percentage_left} style={{ backgroundColor: color2, width: `${percentage}%` }}>
+        <div className={`${styles.column_percentage_left} rectGrow`} style={{ backgroundColor: color2, width: `${percentage}%` }}>
           <span className={styles.text}>{percentage}%</span>
         </div>
         <div className={styles.column_percentage_right}></div>
